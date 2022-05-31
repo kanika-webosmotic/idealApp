0472bca39b3483e99c5ef55009fa36f32b316783
kanika <kanika.webosmotic@gmail.com>
2022-05-31 17:01:43 +0530

    appjs reset

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,13 @@ module.exports = {
         printWidth: 100,
       },
     ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
   plugins: ['prettier'],
 };
