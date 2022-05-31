523efe6ec1916ba61635d939e4a80dc7ba8002d7
WebOsmotic M1 <webosmoticdeveloper@WebOsmotics-Mac-mini-3.local>
2022-05-31 16:03:29 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: ['airbnb', '@react-native-community'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    // You can override any rules you want
+  },
+};
